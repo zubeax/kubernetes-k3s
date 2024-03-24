68ea4902e6bbed5d08ed7b2d4e33d199755d8497
axel <axel@kippel.de>
2024-03-24 11:07:23 +0100

    completed configuration of registry ui

--- configuration/docker-registry/registry/configure-registry.sh
+++ configuration/docker-registry/registry/configure-registry.sh
@@ -5,5 +5,6 @@ ns='docker-registry'
 
 kubectl -n $ns delete configmap docker-registry-config
 kubectl -n $ns create configmap docker-registry-config --from-file=config.yml=$dir/docker-registry-config.yaml
+kubectl -n $ns rollout restart deployment.apps/docker-registry
 
 exit 0
